b9ca7ea32c58f68215a3b3c67c23616b1ec474b2
= <nicostrong@msn.com>
2025-03-13 16:33:54 +0100

    "Update ex00"

--- ex00/Easyfind.tpp
+++ ex00/Easyfind.tpp
@@ -6,7 +6,7 @@
 /*   By: nfordoxc <nfordoxc@42luxembourg.lu>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:44:38 by nfordoxc          #+#    #+#             */
-/*   Updated: 2025/02/25 17:34:44 by nfordoxc         ###   Luxembourg.lu     */
+/*   Updated: 2025/03/13 16:12:56 by nfordoxc         ###   Luxembourg.lu     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,5 @@ typename T::iterator	easyfind( T &container, int value )
 template < typename T >
 typename T::const_iterator	easyfind( const T &container, int value )
 {
-	typename T::const_iterator	it;
-	
-	it = std::find(container.begin(), container.end(), value);
-	if (it == container.end())
-		throw NotFound();
-	return (it);
+	return (easyfind<T>(const_cast<T &>(container), value));
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nfordoxc <nfordoxc@42luxembourg.lu>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:44:01 by nfordoxc          #+#    #+#             */
-/*   Updated: 2025/02/25 17:35:12 by nfordoxc         ###   Luxembourg.lu     */
+/*   Updated: 2025/03/13 16:32:56 by nfordoxc         ###   Luxembourg.lu     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,82 @@
 
 int	main( void )
 {
+	std::vector<int>	vec;
+	std::list<int>		lst;
+	std::deque<int>		deq;
+	
+	/*	we add some value on container	*/
+	for (int i = 0; i < 20; i++)
+		vec.push_back(i * 2);
+	for (int i = 0; i < 20; i++)
+		lst.push_back(i * 3);
+	for (int i = 0; i < 20; i++)
+		deq.push_front(i * 4);
+
+	/*	we print all container	*/
+	std::cout << "Vector: ";
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+
+	std::cout << "List: ";
+	for (std::list<int>::iterator it = lst.begin(); it != lst.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+
+	std::cout << "Deque: ";
+	for (std::deque<int>::iterator it = deq.begin(); it != deq.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+
 	try
 	{
-		std::vector<int> vec;
-		for (int i = 0; i < 10; ++i)
-			vec.push_back(i * 2);
+		std::cout << "✅ Test `easyfind()` on `vector<int>`:" << std::endl;
+		std::vector<int>::iterator	vecIt = easyfind(vec, 6);
+		std::cout << "Found in vector: " << *vecIt << std::endl;
 
-		std::vector<int>::iterator it = easyfind(vec, 4);
-		std::cout << "Found in vector: " << *it << std::endl;
+		std::cout << "✅ Test `easyfind()` on `list<int>`:" << std::endl;
+		std::list<int>::iterator	lstIt = easyfind(lst, 42);
+		std::cout << "Found in list " << *lstIt << std::endl;
 
-		std::list<int> lst;
-		for (int i = 1; i <= 5; ++i)
-			lst.push_back(i * 3);
+		std::cout << "✅ Test `easyfind()` on `deque<int>`:" << std::endl;
+		std::deque<int>::iterator	deqIt = easyfind(deq, 4);
+		std::cout << "Found in deque: " << *deqIt << std::endl;
 
-		std::list<int>::iterator itList = easyfind(lst, 12);
-		std::cout << "Found in list: " << *itList << std::endl;
-
-		easyfind(vec, 99);
-
+		std::cout << "✅ Test Exception 'NotFund' on `vector<int>`:" << std::endl;
+		try
+		{
+			std::vector<int>::iterator	vecIt = easyfind(vec, 19);
+			std::cout << "Found in vector: " << *vecIt << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		std::cout << "✅ Test Exception 'NotFund' on `list<int>`:" << std::endl;
+		try
+		{
+			std::list<int>::iterator	lstIt = easyfind(lst, 60);
+			std::cout << "Found in list " << *lstIt << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		std::cout << "✅ Test Exception 'NotFund' on `deque<int>`:" << std::endl;
+		try
+		{
+			std::deque<int>::iterator	deqIt = easyfind(deq, 1);
+			std::cout << "Found in deque: " << *deqIt << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
 	catch ( const std::exception &e )
 	{
-		std::cerr << e.what() << std::endl;
+		std::cerr << RED << "Something not catching :"<< e.what() << RESET << std::endl;
 	}
-
 	return (0);
 }
